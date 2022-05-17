4522f9fb4550f20d1035a58304426e004b5b888b
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-05-17 16:45:14 -0700

    .

--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -7,6 +7,58 @@ export default class extends AbstractView{
     }
 
     async getHTML() {
-        return "<h1>Countries</h1>";
+        return `
+        <div class="searchbarnfilter">
+            <input type="search" id="searchbar" name="searchbar">
+            <select name="filter" id="filter">
+                <option value="Region">Region</option>
+            </select>    
+        </div>    
+        
+        <div class="country-infos">
+                <div class="flagninfo">
+                    <img src="static/css/Turkey.png" alt="tr" />
+                    <div class="info">
+                        <h3>Turkey</h3>
+                        <h4>Population</h4>
+                        <h4>Region</h4>
+                        <h4>Capital</h4>
+                    </div>
+                </div>
+                <div class="flagninfo">
+                    <img src="static/css/Turkey.png" alt="tr" />
+                    <div class="info">
+                        <h3>Turkey</h3>
+                        <h4>Population</h4>
+                        <h4>Region</h4>
+                        <h4>Capital</h4>
+                    </div>
+                </div>
+                <div class="flagninfo">
+                    <img src="static/css/Turkey.png" alt="tr" />
+                    <div class="info">
+                        <h3>Turkey</h3>
+                        <h4>Population</h4>
+                        <h4>Region</h4>
+                        <h4>Capital</h4>
+                    </div>
+                </div>
+                <div class="flagninfo">
+                    <img src="static/css/Turkey.png" alt="tr" />
+                    <div class="info">
+                        <h3>Turkey</h3>
+                        <h4>Population</h4>
+                        <h4>Region</h4>
+                        <h4>Capital</h4>
+                    </div>
+                </div>
+            
+
+        </div>
+        `
+            
+        
+
+        ;
     }
 }
