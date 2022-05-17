a328af9419958a25cb2faa870acd04902ddd989e
Mauricio Tomaz <mtomaz@whiteoutsolutions.com>
2022-05-17 14:48:36 -0700

    Initial commit

--- frontend/static/js/views/Homepage.js
+++ frontend/static/js/views/Homepage.js
@@ -8,7 +8,34 @@ export default class extends AbstractView{
 
     async getHTML() {
         return `
-            <h1>Homepage</h1>
+            <main>
+                <section class="hero">
+                    <h2>Explore the world</h2>
+                    <button type="button">See more!</button>
+                </section>
+                <section class="authors">
+                    <h2>Meet the authors</h2>
+                    <div class="authors-container">
+                        <div class="author">
+                            <img src="emir.jpeg" alt="Emir" />
+                            <h3>Emir</h3>
+                        </div>
+                        <div class="author">
+                            <img src="ozan.jpeg" alt="Ozan" />
+                            <h3>Ozan</h3>
+                        </div>
+                        <div class="author">
+                            <img src="mauricio.jpeg" alt="Mauricio" />
+                            <h3>Mauricio</h3>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <footer>
+                <p>
+                    Final Project Webdev I - Cornerstone International College | Emir, Ozan and Maurício.
+                </p>
+            </footer>
         `;
     }
 }
