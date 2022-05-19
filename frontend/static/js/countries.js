dc365dfa14034d097d8e144cc90d76166eb899c2
atesmen <63913174+atesmen@users.noreply.github.com>
2022-05-19 15:40:56 -0700

    country view page

--- frontend/static/js/countries.js
+++ frontend/static/js/countries.js
@@ -11,6 +11,28 @@ export function getCountries() {
     return JSON.parse(countries);
 };
 
+export function getCountryByCode(countryCode){
+    const countries = getCountries();
+    for (let i = 0; i < countries.length; i++){
+        const country = countries[i];
+        if (country.cca3 === countryCode.toUpperCase()) {
+            return country;
+        }
+    }
+}
 
-
-
+export function getBorders(borderCountries){
+    const borderCode = []
+    if(!borderCountries){
+        return []
+    }
+    for (let i = 0; i < borderCountries.length; i++){
+        const country = getCountryByCode(borderCountries[i]);
+        borderCode.push(`
+            <a href="/countries/${country.cca3}">
+                <button id="borbtn">${country.name.common}</button>
+            </a>
+        `)
+    }
+    return borderCode.join('');
+}
--- frontend/static/js/views/CountryView.js
+++ frontend/static/js/views/CountryView.js
@@ -1,45 +1,47 @@
+import { getBorders, getCountryByCode } from "../countries.js";
 import AbstractView from "./AbstractView.js";
 
 export default class extends AbstractView{
     constructor(params) {
         super(params);
         this.setTitle("Country");
-
-        console.log(getCountry())
         
     }
-
+    
     async getHTML() {
-        console.log(this.params.id);
+        const country = getCountryByCode(this.params.id);
         return `
-        <div class="bigFlag" id="cFlag"></div>
+        <div class="bigFlag" id="cFlag"> <img src="${country.flags.svg}" alt=""> </div>
     <div class="countryTitle">
-        <h1 id="cName">canada</h1>
+        <h1 id="cName">${country.name.common}</h1>
     </div>
     <div class="countryInfo" id="cInfo">
         <div class="countryInfo-Left">
-            <div class="countryInfo-nn"><h3>Native Name :</h3> <h3 id="nN"></h3></div>
-            <div class="countryInfo-pop"><h3>Population :</h3> <h3 id="pop"></h3></div>
-            <div class="countryInfo-reg"><h3>Region :</h3> <h3 id="reg"></h3></div>
-            <div class="countryInfo-sreg"><h3>Subregion :</h3> <h3 id="sreg"></h3></div>   
+            <div class="countryInfo-nn"><h3>Native Name :</h3> <h3 id="nN">${Object.entries(country.name.nativeName)[0][1].official}
+            </h3></div>
+            <div class="countryInfo-pop"><h3>Population :</h3> <h3 id="pop"> ${country.population}</h3></div>
+            <div class="countryInfo-reg"><h3>Region :</h3> <h3 id="reg"> ${country.region}</h3></div>
+            <div class="countryInfo-sreg"><h3>Subregion :</h3> <h3 id="sreg"> ${country.subregion}</h3></div>   
         </div>
         <div class="countryInfo-Right">
-            <div class="countryInfo-cap"><h3>Capital :</h3> <h3 id="cap"></h3></div>
-            <div class="countryInfo-tld"><h3>Top Level Domain :</h3> <h3 id="tld"></h3></div>
-            <div class="countryInfo-cur"><h3>Currencies :</h3> <h3 id="cur"></h3></div>
-            <div class="countryInfo-lan"><h3>Languages :</h3> <h3 id="lan"></h3></div>  
+            <div class="countryInfo-cap"><h3>Capital :</h3> <h3 id="cap"> ${country.capital}</h3></div>
+            <div class="countryInfo-tld"><h3>Top Level Domain :</h3> <h3 id="tld"> ${country.tld}</h3></div>
+            <div class="countryInfo-cur"><h3>Currencies : </h3> <h3 id="cur">
+            ${Object.entries(country.currencies)[0][1].name} (${Object.entries(country.currencies)[0][1].symbol})</h3></div>
+            <div class="countryInfo-lan"><h3>Languages :</h3> <h3 id="lan">${Object.values(country.languages).join(', ')
+        }</h3></div>  
         </div>
     </div>
     <div class="bordercountry">
         <h1>Border Countries</h1>
         <div class="bordercountry-container">
             <div class="bordercountry-btn">
-                <button id="borbtn"></button> 
+            ${getBorders(country.borders)}
             </div>
         </div>
     </div>
     <div class="goback">
-        <button>Go Back To Countries</button>
+    <a href="/countries"><button>Go Back To Countries</button></a>  
     </div> 
         `;
     }
